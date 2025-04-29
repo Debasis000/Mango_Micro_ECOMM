b0625d60a3c3dc54a5d1807e5a2b3943204d56f6
Debasis Dalai <debasisdalai022@gmail.com>
2025-04-30 00:01:06 +0530

    Add project files.

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private ResponseDto _response;
+        private IMapper _mapper;
+        public CouponAPIController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+            _response = new ResponseDto();
+        }
+        [HttpGet]
+        public async Task<ResponseDto>  Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = await _context.Coupons.ToListAsync();
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(objList);
+                
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ResponseDto> Get(int id)
+        {
+            try
+            {
+                Coupon objList = await _context.Coupons.FirstAsync(u => u.CouponId == id);
+                _response.Result = _mapper.Map<CouponDto>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
---
+++ Mango.Services.CouponAPI/Program.cs
@@ -0,0 +1,62 @@
+
+using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("appicon"));
+            });
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            builder.Services.AddSingleton(mapper);
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            ApplicaMigration();
+
+            app.Run();
+
+            void ApplicaMigration()
+            {
+                using (var scope = app.Services.CreateScope())
+                {
+                    var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                    if(_db.Database.GetPendingMigrations().Count() > 0)
+                    {
+                        _db.Database.Migrate();
+                    }
+                }
+            }
+        }
+    }
+}
